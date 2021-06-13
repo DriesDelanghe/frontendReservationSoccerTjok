6f35ec69092334c13b119f0b8cdceffadfdf377f
Dries <delanghe.dries.1998@gmail.com>
2021-06-13 17:37:23 +0200

    displaying error message on UI

--- js/fetchingScript.js
+++ js/fetchingScript.js
@@ -31,10 +31,36 @@ const sendFormDataToServer = (url, data) => {
             const response = JSON.parse(xhr.responseText);
             console.log(response)
         }
+        if (xhr.status >= 400 && xhr.status < 600){
+            const div = createErrorMessage(`Something went wrong, please try again`);
+            if (div) {
+                document.getElementById(`errorDiv`).appendChild(div);
+            }
+        }
     };
 
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
 
     xhr.send(JSON.stringify(data));
+}
+
+const createErrorMessage = (text) => {
+    const errorDiv = document.getElementById(`serverErrorMessage`)
+    if (errorDiv){
+        return null;
+    }
+    const div = document.createElement(`div`);
+    div.id = `serverErrorMessage`
+    div.className = `alert alert-danger d-flex justify-content-between flex-nowrap position-fixed w-100`;
+    div.style.zIndex = `1024`;
+    const p = document.createElement(`p`);
+    p.className = `m-1 lead`;
+    p.innerHTML = text;
+    const button = document.createElement(`button`);
+    button.className = `btn-close`;
+    button.onclick = () => div.remove();
+    div.appendChild(p);
+    div.appendChild(button);
+    return div;
 }
